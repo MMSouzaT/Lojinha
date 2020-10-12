e47d7f39abb25e363adaaa8e459d09adb81ef844
Márcio Souza <marcio.messiassouza@gmail.com>
2020-10-12 17:55:04 -0300

    insersão de novos métodos

--- src/test/java/testes/LojinhaAPITest.java
+++ src/test/java/testes/LojinhaAPITest.java
@@ -28,53 +28,157 @@ public class LojinhaAPITest {
                         .path("data.token");
 
     }
-    @Test
-    public void testBuscarDadosDeUmProdutoEspecifico(){
-
-        RestAssured
-                .given()
-                    .header("token", token)
-                .when()
-                    .get("produto/8397")
-                .then()
-                    .assertThat()
-                        .statusCode(200)
-                        .body("data.produtonome", Matchers.equalTo("Casa"))
-                        .body("data.componentes[0].componentenome", Matchers.equalTo("TV"))
-                        .body("message", Matchers.equalTo("Detalhando dados do produto"));
-
-    }
 
     @Test
     public void cadastrandoUmProduto(){
         RestAssured
                 .given()
+                .header("token", token)
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "    \"produtonome\": \"Computador\",\n" +
+                        "    \"produtovalor\": 1300.00,\n" +
+                        "    \"produtocores\": [\n" +
+                        "        \"Preto\"\n" +
+                        "    ],\n" +
+                        "    \"componentes\": [\n" +
+                        "        {\n" +
+                        "            \"componentenome\": \"Caixa de som\",\n" +
+                        "            \"componentequantidade\": 2\n" +
+                        "        },\n" +
+                        "        {\n" +
+                        "            \"componentenome\": \"Teclado\",\n" +
+                        "            \"componentequantidade\": 1\n" +
+                        "        }\n" +
+                        "    ]\n" +
+                        "}")
+                .when()
+                .post("produto")
+                .then()
+                .assertThat()
+                .statusCode(201)
+                .body("data.produtonome", Matchers.equalTo("Computador"))
+                .body("data.componentes[0].componentenome", Matchers.equalTo("Caixa de som"))
+                .body("message", Matchers.equalTo("Produto adicionado com sucesso"));
+    }
+
+    @Test
+    public void testBuscarTodosProdutos(){
+        RestAssured
+            .given()
+                .header("token", token)
+            .when()
+                .get("produto")
+            .then()
+                .assertThat()
+                    .statusCode(200)
+                    .body("data.produtoid[0]", Matchers.equalTo(8397))
+                    .body("message", Matchers.equalTo("Listagem de produtos realizada com sucesso"));
+    }
+
+    @Test
+    public void testBuscarDadosDeUmProdutoEspecifico(){
+
+        RestAssured
+                .given()
+                .header("token", token)
+                .when()
+                .get("produto/8397")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body("data.produtonome", Matchers.equalTo("Casa"))
+                .body("data.componentes[0].componentenome", Matchers.equalTo("TV"))
+                .body("message", Matchers.equalTo("Detalhando dados do produto"));
+
+    }
+
+    @Test
+    public void testBuscarComponenteDoProdutoComFiltro(){
+
+        RestAssured
+                .given()
+                .header("token", token)
+                .when()
+                .get("produto/8397?=TV")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body("data.produtoid", Matchers.equalTo(8397))
+                .body("message", Matchers.equalTo("Detalhando dados do produto"));
+    }
+
+    @Test
+    public void testAlterarProduto(){
+        RestAssured
+                .given()
                     .header("token", token)
                     .contentType(ContentType.JSON)
                     .body("{\n" +
-                            "    \"produtonome\": \"Computador\",\n" +
-                            "    \"produtovalor\": 1300.00,\n" +
-                            "    \"produtocores\": [\n" +
-                            "        \"Preto\"\n" +
-                            "    ],\n" +
-                            "    \"componentes\": [\n" +
-                            "        {\n" +
-                            "            \"componentenome\": \"Caixa de som\",\n" +
-                            "            \"componentequantidade\": 2\n" +
-                            "        },\n" +
-                            "        {\n" +
-                            "            \"componentenome\": \"Teclado\",\n" +
-                            "            \"componentequantidade\": 1\n" +
-                            "        }\n" +
-                            "    ]\n" +
+                            " \"produtonome\": \"Massarico\",\n" +
+                            " \"produtovalor\": 500.00,\n" +
+                            " \"produtocores\": [\n" +
+                            " \"Dourado\"\n" +
+                            " ],\n" +
+                            " \"componentes\": [\n" +
+                            " {\n" +
+                            " \"componentenome\": \"Tomada\",\n" +
+                            " \"componentequantidade\": 1\n" +
+                            " }\n" +
+                            " ]\n" +
                             "}")
                 .when()
-                    .post("produto")
+                    .put("produto/8398")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body("data.produtonome", Matchers.equalTo("Massarico"))
+                .body("data.componentes[0].componentenome", Matchers.equalTo("Tomada"))
+                .body("message", Matchers.equalTo("Produto alterado com sucesso"));
+    }
+
+    @Test
+    public void testAlterarProdutoDeVolta(){
+        RestAssured
+                .given()
+                .header("token", token)
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        " \"produtonome\": \"Elevador\",\n" +
+                        " \"produtovalor\": 1500.00,\n" +
+                        " \"produtocores\": [\n" +
+                        " \"Cinza\"\n" +
+                        " ],\n" +
+                        " \"componentes\": [\n" +
+                        " {\n" +
+                        " \"componentenome\": \"Botões\",\n" +
+                        " \"componentequantidade\": 12\n" +
+                        " }\n" +
+                        " ]\n" +
+                        "}")
+                .when()
+                .put("produto/8398")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body("data.produtonome", Matchers.equalTo("Elevador"))
+                .body("data.componentes[0].componentenome", Matchers.equalTo("Botões"))
+                .body("message", Matchers.equalTo("Produto alterado com sucesso"));
+    }
+
+
+    @Test
+    public void testBuscarDadosDosProdutos(){
+
+        RestAssured
+                .given()
+                    .header("token", token)
+                .when()
+                    .get("produto")
                 .then()
                     .assertThat()
-                        .statusCode(201)
-                        .body("data.produtonome", Matchers.equalTo("Computador"))
-                        .body("data.componentes[0].componentenome", Matchers.equalTo("Caixa de som"))
-                        .body("message", Matchers.equalTo("Produto adicionado com sucesso"));
-        }
+                        .statusCode(200)
+                        .body("message", Matchers.equalTo("Listagem de produtos realizada com sucesso"));
+    }
+
 }
